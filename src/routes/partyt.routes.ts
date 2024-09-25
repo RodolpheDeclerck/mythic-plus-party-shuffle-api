41a3309492364f9c6e0f6a6955af364cfa666bbf
Rodeclerck <50379466+RodolpheDeclerck@users.noreply.github.com>
2024-09-25 15:09:30 -0400

    add parties shuffled to web socket

--- src/routes/partyt.routes.ts
+++ src/routes/partyt.routes.ts
@@ -7,14 +7,13 @@ const router = Router();
 // Route pour mélanger et créer de nouveaux groupes
 router.get('/parties/shuffle', async (req, res) => {
     try {
-        const shuffledParties = await partyController.shuffleParties; // Retourne les groupes mélangés
+        const shuffledParties = await partyController.shuffleParties(req, res); // Retourne les groupes mélangés
         io.emit('parties-shuffled');
         res.status(200).json(shuffledParties); // Renvoie les groupes mélangés
     } catch (error: any) {
         res.status(500).json({ message: 'Failed to shuffle parties', error });
     }
 });
-
 
 
 // Route pour récupérer les groupes actuels à partir de Redis
