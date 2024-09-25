a8f8849f29741c4fee9fb44e342f7e7ea174468f
Rodeclerck <50379466+RodolpheDeclerck@users.noreply.github.com>
2024-09-25 14:20:55 -0400

    add parties shuffled to web socket

--- src/routes/partyt.routes.ts
+++ src/routes/partyt.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from 'express';
 import { partyController } from '../controllers/party.controller.js';
+import { io } from '../app.js';  // Importer l'instance de io pour émettre les événements
 
 const router = Router();
 
 // Route pour mélanger et créer de nouveaux groupes
 router.get('/parties/shuffle', partyController.shuffleParties);
+
+// PUT request to update a character by ID
+router.put('/parties/shuffle', async (req, res) => {
+    try {
+      await partyController.shuffleParties;
+      io.emit('parties-shuffled');  // Émettre un événement après la mise à jour
+    } catch (error: any) {
+      res.status(500).json({ message: 'Failed to update character', error });
+    }
+  });
 
 // Route pour récupérer les groupes actuels à partir de Redis
 router.get('/parties', partyController.getParties);
